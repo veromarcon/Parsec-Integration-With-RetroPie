dd1616e87c5f50d086c2ccce22d9921ffda7eaa8
veromarcon <veronica_marcon@hotmail.com>
2023-09-23 22:38:02 -0600

    change repo

--- curlsetup.sh
+++ curlsetup.sh
@@ -1,6 +1,6 @@
 sudo apt install git -y
 TMPDIR=$(mktemp -d)
 cd "$TMPDIR" || exit
-git clone https://github.com/Kozova1/Parsec-Integration-With-RetroPie.git ./
+git clone https://github.com/veromarcon/Parsec-Integration-With-RetroPie.git ./
 ./setup.sh
 rm -rf "$TMPDIR"
